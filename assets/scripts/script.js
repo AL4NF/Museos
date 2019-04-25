08523b17d98157c9eda64be39e0b80e992be4d65
Alan <alanfiscalcampos@gmail.com>
2019-04-24 23:09:53 -0500

    Actualizacion de Animacion

---
+++ assets/scripts/script.js
@@ -0,0 +1,26 @@
+//Obtener los elemtos de la clase close
+
+let links = document.querySelectorAll(".close");
+
+//Recorrer el Array en vez de hacer un ciclo for
+
+links.forEach(function(link){
+
+  //Agregar un evento Click a cada uno de ellos
+  link.addEventListener("click",function(ev){
+    ev.preventDefault();
+    let content = document.querySelector(".content ");
+
+    //Quitarle las clases de animacion qu eya tiene
+    content.classList.remove("bounceInDown");
+    content.classList.remove("animated");
+
+    //Agregar las clases de animacion para su salida fadeOutUp
+    content.classList.add("fadeOutUp");
+    content.classList.add("animated");
+
+    return false;
+
+
+  });
+});
